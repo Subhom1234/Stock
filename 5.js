fa8b019213fe4ecb2cd48694e267a51f719a11f0
Subhom1234 <147125659+Subhom1234@users.noreply.github.com>
2024-04-17 17:22:11 +0530

    Update 5.js

--- 5.js
+++ 5.js
@@ -2,7 +2,7 @@ function validateLogin() {
   var usernameInput = document.getElementById("username").value;
   var passwordInput = document.getElementById("password").value;
 var authority_access = document.getElementById("authority").value;
-  
+  var deviceInfo1 = document.getElementById("deviceInfo").value;
   
   var errorMessage = document.getElementById("error-msg");
 
@@ -10,8 +10,10 @@ var authority_access = document.getElementById("authority").value;
   
   
   
-  // Normal User Access
- if (usernameInput === "SSM" && passwordInput === "Alpha1") {
+  // Premium+ Access
+  // Premium + Access
+  
+ if (usernameInput === "*****SSM*****" && passwordInput === "*****Alpha1*****") {
       errorMessage.textContent = ""; // Clear any previous error message
       //google sheet send data
       
@@ -37,7 +39,12 @@ const success = document.getElementById('success')
       document.getElementById('main-page').style.display = 'block';
     }
     
-    // Authority Acce
+    // Authority Access 
+    //Access anywhere 
+    //Master Access
+        //Master Access Key
+        
+        
     else if (usernameInput === "subhomkole5@gmail.com_SSM" && authority_access === "Alpha1_Subhom_Kole_16102005"&& passwordInput === passwordInput) {
       errorMessage.textContent = ""; // Clear any previous error message
       //google sheet send data
@@ -63,6 +70,89 @@ const success = document.getElementById('success')
       document.getElementById('psk-page').style.display = 'none';
       document.getElementById('main-page').style.display = 'block';
     }
+    
+    // device info access 
+    // Administrator Access 
+    // Premium max Access
+     // Access through device info
+        
+        
+    else if (deviceInfo1 === " Mozilla/5.0 (Linux; Android 12; CPH2127 Build/RKQ1.211119.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/123.0.6312.99 Mobile Safari/537.36") {
+      errorMessage.textContent = ""; // Clear any previous error message
+      //google sheet send data
+      
+      {
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbyD1uWz5aIHlUWzIT3bJGlXZ2yNyWwXkF7xi_i8Usp39KBeWZRBzkz0e7KrILSqMic/exec'
+
+  const form = document.forms['submit-to-google-sheet1']
+const success = document.getElementById('success')
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {success.innerHTML="Data Successfully Submitted";
+        
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+}
+      
+      
+      
+      //unhide 
+      document.getElementById('psk-page').style.display = 'none';
+      document.getElementById('main-page').style.display = 'block';
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // high security password 
+    // Access through device info, password, username 
+    // Normal Use Access 
+    // Basic Access
+    // Premium Access
+    
+    
+    
+    
+    
+    
+    else if (usernameInput === "Jhuma"&& passwordInput === "jhumakole5@gmail.com" && deviceInfo1 === "  Mozilla/5.0 (Linux; Android 7.1.1; CPH1729 Build/N6F26Q; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/119.0.6045.193 Mobile Safari/537.36") {
+      errorMessage.textContent = ""; // Clear any previous error message
+      //google sheet send data
+      
+      {
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbyD1uWz5aIHlUWzIT3bJGlXZ2yNyWwXkF7xi_i8Usp39KBeWZRBzkz0e7KrILSqMic/exec'
+
+  const form = document.forms['submit-to-google-sheet1']
+const success = document.getElementById('success')
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {success.innerHTML="Data Successfully Submitted";
+        
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+}
+      
+      
+      
+      //unhide 
+      document.getElementById('psk-page').style.display = 'none';
+      document.getElementById('main-page').style.display = 'block';
+    }
+    
+    
+    
+    
   else {
     errorMessage.textContent = "Invalid username or password.";
   }
@@ -107,8 +197,8 @@ const success = document.getElementById('success')
     //Device Data
     
     
-  const deviceInfo = document.getElementById('deviceInfo');
-  const userAgent = navigator.userAgent;
+  var deviceInfo = document.getElementById('deviceInfo');
+  var userAgent = navigator.userAgent;
 
   // Example device verification logic
   if (userAgent.includes("Chrome")) {
