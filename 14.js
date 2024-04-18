a7993b097bc177db1f3417d8384503bd5fe8ad9d
Subhom1234 <147125659+Subhom1234@users.noreply.github.com>
2024-04-18 19:07:28 +0530

    Update 14.js

--- 14.js
+++ 14.js
@@ -369,7 +369,28 @@ document.getElementById('res2').textContent= "Result : " + persentage + "% ( " +
     
     
   }
+  
+  
+  {
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbw7elEk9mmqAL0fxXxPqXv9ItbCcPy5lUfDN2WHUIKByMtiDBr0pEun5XtIlbCbPlVV/exec'
+
+  const form = document.forms['submit-to-google-sheet']
+const success = document.getElementById('success')
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {success.innerHTML="Data Successfully Submitted";
+        setTimeout(function(){
+          success.innerHTML="";},5000)
+        form.reset();
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
 }
-{
+  
+  
+  
+  
   
 }
+  
